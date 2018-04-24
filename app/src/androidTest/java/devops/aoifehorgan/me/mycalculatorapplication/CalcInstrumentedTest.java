993fbc429ec820dbc294ada5b6e661e7b130da71
Aoife Horgan <t00180243@gmail.com>
2018-04-24 13:50:22 +0100

    Adding Tests

---
+++ app/src/androidTest/java/devops/aoifehorgan/me/mycalculatorapplication/CalcInstrumentedTest.java
@@ -0,0 +1,70 @@
+package devops.aoifehorgan.me.mycalculatorapplication;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+
+public class CalcInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<CalcActivity> mActivityRule =
+            new ActivityTestRule<>(CalcActivity.class);
+
+    @Test
+    public void testAddition() {
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.buttonAdd)).perform(click());
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.textViewAns)).check(matches(withText("2+2")));
+        onView(withId(R.id.buttonEql)).perform(click());
+        onView(withId(R.id.textViewAns)).check(matches(withText("4")));
+    }
+    @Test
+    public void testSubtraction() {
+        onView(withId(R.id.button6)).perform(click());
+        onView(withId(R.id.buttonSub)).perform(click());
+        onView(withId(R.id.button1)).perform(click());
+        onView(withId(R.id.textViewAns)).check(matches(withText("6-1")));
+        onView(withId(R.id.buttonEql)).perform(click());
+        onView(withId(R.id.textViewAns)).check(matches(withText("5")));
+    }
+    @Test
+    public void testMultiply() {
+        onView(withId(R.id.button3)).perform(click());
+        onView(withId(R.id.buttonMul)).perform(click());
+        onView(withId(R.id.button1)).perform(click());
+        onView(withId(R.id.textViewAns)).check(matches(withText("3x1")));
+        onView(withId(R.id.buttonEql)).perform(click());
+        onView(withId(R.id.textViewAns)).check(matches(withText("3")));
+    }
+
+    @Test
+    public void testDivision() {
+        // Type text and then press the button.
+        onView(withId(R.id.button8)).perform(click());
+        onView(withId(R.id.buttonDiv)).perform(click());
+        onView(withId(R.id.button4)).perform(click());
+        onView(withId(R.id.textViewAns)).check(matches(withText("8/4")));
+        onView(withId(R.id.buttonEql)).perform(click());
+        onView(withId(R.id.textViewAns)).check(matches(withText("2")));
+    }
+
+}
+
+
---
+++ app/src/test/java/devops/aoifehorgan/me/mycalculatorapplication/CalcUnitTest.java
@@ -0,0 +1,90 @@
+package devops.aoifehorgan.me.mycalculatorapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class CalcUnitTest {
+    private CalcActivity activity = new CalcActivity();
+    private String add = CalcActivity.ADD;
+    private String minus = CalcActivity.SUB;
+    private String mul = CalcActivity.MUL;
+    private String div = CalcActivity.DIV;
+    @Test
+    public void applyOperation() throws Exception {
+       checkAddition();
+       checkSubtraction();
+       checkMultiplication();
+       checkDivision();
+    }
+    @Test
+    public void checkAddition() throws Exception {
+        double actual = activity.applyOperation(add,2,2);
+        double expected = 4;
+        assertEquals("Failed",expected,actual,0.001);
+    }
+    @Test
+    public void checkSubtraction() throws Exception {
+        double actual = activity.applyOperation(minus,2,2);
+        double expected = 0;
+        assertEquals("Failed",expected,actual,0.001);
+    }
+    @Test
+    public void checkMultiplication() throws Exception {
+        double actual = activity.applyOperation(mul,2,4);
+        double expected = 8;
+        assertEquals("Failed",expected,actual,0.001);
+    }
+    @Test
+    public void checkDivision() throws Exception {
+        double actual = activity.applyOperation(div,4,2);
+        double expected = 2;
+        assertEquals("Failed",expected,actual,0.001);
+    }
+
+
+
+    @Test
+    public void onCreateOptionsMenu() throws Exception {
+        assertTrue(true);
+    }
+
+    @Test
+    public void registerKey() throws Exception {
+        activity.safelyPlaceOperator("0");
+        activity.safelyPlaceOperator("1");
+        activity.safelyPlaceOperator(minus);
+        activity.safelyPlaceOperator(add);
+
+    }
+
+    @Test
+    public void display() throws Exception {
+
+    }
+
+    @Test
+    public void endsWithNumber() throws Exception {
+        boolean actual = activity.endsWithNumber();
+        //int expected = false;
+        assertFalse("2222",actual);
+    }
+
+    @Test
+    public void endsWithOperator() throws Exception {
+        boolean actual =activity.endsWithOperator();
+        assertFalse(actual);
+    }
+
+
+
+    @Test
+    public void resetCalculator() throws Exception {
+        activity.resetCalculator();
+
+    }
+
+
+}
