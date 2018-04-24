25b82847c8b32541fbfee5ddad4c8621a86a9c1c
Aoife Horgan <t00180243@gmail.com>
2018-04-24 23:02:06 +0100

    Adding Tests

--- app/src/androidTest/java/devops/aoifehorgan/me/mycalculatorapplication/CalcInstrumentedTest.java
+++ app/src/androidTest/java/devops/aoifehorgan/me/mycalculatorapplication/CalcInstrumentedTest.java
@@ -16,7 +16,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Instrumented test, which will execute on an Android device.
- *
+ **
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
